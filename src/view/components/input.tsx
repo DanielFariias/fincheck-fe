38e683d2ce4c9198fc26428d26f1b657f7b6d25e
DanielFariias <danielfariias15@gmail.com>
2024-02-08 23:00:11 -0300

    feat: Refactor Input component to include error handling and ref

--- src/view/components/input.tsx
+++ src/view/components/input.tsx
@@ -1,27 +1,46 @@
-import { ComponentProps } from 'react'
+import { CrossCircledIcon } from '@radix-ui/react-icons'
+import { ComponentProps, forwardRef } from 'react'
+import { cn } from '../../app/utils/cn'
 
 interface InputProps extends ComponentProps<'input'> {
   name: string
+  error?: string
 }
 
-export function Input({ id, name, placeholder, ...props }: InputProps) {
-  const inputId = id ?? name
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ id, name, placeholder, error, className, ...props }: InputProps, ref) => {
+    const inputId = id ?? name
 
-  return (
-    <div className="relative">
-      <input
-        {...props}
-        id={inputId}
-        name={name}
-        className="bg-white w-full rounded-lg border placeholder-shown:pt-0 pt-4 border-gray-500 px-3 h-[52px] text-gray-800 focus:border-gray-800 outline-none transition-all peer"
-        placeholder=" "
-      />
-      <label
-        htmlFor={name}
-        className="absolute text-xs top-2 left-[13px] pointer-events-none text-gray-700 peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 transition-all"
-      >
-        {placeholder}
-      </label>
-    </div>
-  )
-}
+    return (
+      <div className="relative">
+        <input
+          ref={ref}
+          {...props}
+          id={inputId}
+          name={name}
+          className={cn(
+            'bg-white w-full rounded-lg border placeholder-shown:pt-0 pt-4 border-gray-500 px-3 h-[52px] text-gray-800 focus:border-gray-800 outline-none transition-all peer',
+            error && 'border-red-900 focus:border-red-900',
+            className,
+          )}
+          placeholder=" "
+        />
+        <label
+          htmlFor={name}
+          className="absolute text-xs top-2 left-[13px] pointer-events-none text-gray-700 peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 transition-all"
+        >
+          {placeholder}
+        </label>
+
+        {error && (
+          <div className="flex gap-2 items-center mt-2 text-red-900">
+            <CrossCircledIcon />
+            <span className="text-xs">{error}</span>
+          </div>
+        )}
+      </div>
+    )
+  },
+)
+
+Input.displayName = 'Input'
