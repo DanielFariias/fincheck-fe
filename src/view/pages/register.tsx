0152cbc457f940945e5e0ebd6fac7188cbdbf50a
DanielFariias <danielfariias15@gmail.com>
2024-02-08 22:05:40 -0300

    feat: create initial login and register pages

--- src/view/pages/register.tsx
+++ src/view/pages/register.tsx
@@ -1,3 +1,34 @@
+import { Link } from 'react-router-dom'
+import { Input } from '../components/input'
+import { Button } from '../components/button'
+
 export function Register() {
-  return <div>Register</div>
+  return (
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold tracking-[-1px]">Crie sua conta</h1>
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Já possui uma conta?
+          </span>
+          <Link
+            to={''}
+            className="text-green-900 font-medium tracking-[-0.5px]"
+          >
+            Faça Login
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input name="name" placeholder="Nome" />
+        <Input name="email" type="email" placeholder="Email" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <Button type="submit" className="mt-2">
+          Criar conta
+        </Button>
+      </form>
+    </>
+  )
 }
