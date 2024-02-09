876b0dc792306422ccd396e6a458d1198fbe2dcd
DanielFariias <danielfariias15@gmail.com>
2024-02-09 11:49:15 -0300

    feat: Add AuthContext component

---
+++ src/app/contexts/auth-context.tsx
@@ -0,0 +1,73 @@
+import {
+  ReactNode,
+  createContext,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
+
+import { localStorageKeys } from '../config/local-storage-keys'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { userService } from '../services/user-service'
+import toast from 'react-hot-toast'
+import { LaunchScreen } from '../../view/components/launch-screen'
+
+interface IAuthContextProps {
+  signedIn: boolean
+  signIn: (accessToken: string) => void
+  signOut: VoidFunction
+}
+
+export const AuthContext = createContext({} as IAuthContextProps)
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [signedIn, setSignedIn] = useState<boolean>(() => {
+    const storedAccessToken = localStorage.getItem(
+      localStorageKeys.ACCESS_TOKEN,
+    )
+
+    return !!storedAccessToken
+  })
+  const queryClient = useQueryClient()
+
+  const { isError, isFetching, isSuccess } = useQuery({
+    queryKey: ['users', 'me'],
+    queryFn: () => userService.me(),
+    enabled: signedIn,
+    staleTime: Infinity,
+  })
+
+  const signIn = useCallback((accessToken: string) => {
+    localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken)
+
+    setSignedIn(true)
+  }, [])
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem(localStorageKeys.ACCESS_TOKEN)
+    queryClient.removeQueries({ queryKey: ['users', 'me'] })
+
+    setSignedIn(false)
+  }, [queryClient])
+
+  useEffect(() => {
+    if (isError) {
+      toast.error('Sessão expirada, faça login novamente!')
+      signOut()
+    }
+  }, [isError, signOut])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signedIn: isSuccess && signedIn,
+        signIn,
+        signOut,
+      }}
+    >
+      <LaunchScreen isActive={isFetching} />
+
+      {!isFetching && children}
+    </AuthContext.Provider>
+  )
+}
