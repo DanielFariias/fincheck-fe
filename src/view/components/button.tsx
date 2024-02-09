e345b992fb74eac1b58b82a11cd674ec84ffbf42
DanielFariias <danielfariias15@gmail.com>
2024-02-09 11:50:09 -0300

    Add isLoading prop to Button component

--- src/view/components/button.tsx
+++ src/view/components/button.tsx
@@ -1,16 +1,29 @@
 import { ComponentProps } from 'react'
 import { cn } from '../../app/utils/cn'
+import { Spinner } from './spinner'
 
-interface ButtonProps extends ComponentProps<'button'> {}
+interface ButtonProps extends ComponentProps<'button'> {
+  isLoading?: boolean
+}
 
-export function Button({ className, ...props }: ButtonProps) {
+export function Button({
+  className,
+  isLoading,
+  disabled,
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
       {...props}
+      disabled={disabled || isLoading}
       className={cn(
-        'bg-teal-900 hover:bg-teal-800 w-full px-6 h-12 rounded-2xl text-white font-medium active:bg-teal-700 disabled:text-gray-500 disabled:bg-gray-200 disabled:cursor-not-allowed transition-all',
+        'bg-teal-900 hover:bg-teal-800 w-full px-6 h-12 rounded-2xl text-white font-medium active:bg-teal-700 disabled:text-gray-500 disabled:bg-gray-200 disabled:cursor-not-allowed transition-all flex items-center justify-center',
         className,
       )}
-    />
+    >
+      {!isLoading && children}
+      {isLoading && <Spinner className="w-6 h-6 fill-teal-700" />}
+    </button>
   )
 }
