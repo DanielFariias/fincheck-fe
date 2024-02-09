17917142ec7a621d9af759391414a978a41ded01
DanielFariias <danielfariias15@gmail.com>
2024-02-09 11:47:30 -0300

    feat: Add app.tsx and remove index.tsx

---
+++ src/app.tsx
@@ -0,0 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Router } from './router'
+import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './app/contexts/auth-context'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
+  )
+}
