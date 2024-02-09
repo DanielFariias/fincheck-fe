cffb83ed5d3a67f492a5e8db00c6b3341246110d
DanielFariias <danielfariias15@gmail.com>
2024-02-08 23:01:20 -0300

    refactor: Button component to use cn utility

--- src/view/components/button.tsx
+++ src/view/components/button.tsx
@@ -1,12 +1,16 @@
 import { ComponentProps } from 'react'
+import { cn } from '../../app/utils/cn'
 
 interface ButtonProps extends ComponentProps<'button'> {}
 
-export function Button(props: ButtonProps) {
+export function Button({ className, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className="bg-teal-900 hover:bg-teal-800 w-full px-6 h-12 rounded-2xl text-white font-medium active:bg-teal-700 disabled:text-gray-500 disabled:bg-gray-200 disabled:cursor-not-allowed transition-all"
+      className={cn(
+        'bg-teal-900 hover:bg-teal-800 w-full px-6 h-12 rounded-2xl text-white font-medium active:bg-teal-700 disabled:text-gray-500 disabled:bg-gray-200 disabled:cursor-not-allowed transition-all',
+        className,
+      )}
     />
   )
 }
