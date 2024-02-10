7548eb5191d1b2a5c102c0af9878500c1360a3f8
DanielFariias <danielfariias15@gmail.com>
2024-02-10 17:48:35 -0300

    feat: Refactor dashboard layout and add DashboardProvider

--- src/view/pages/dashboard/index.tsx
+++ src/view/pages/dashboard/index.tsx
@@ -1,24 +1,27 @@
 import { Logo } from '../../components/logo'
 import { UserMenu } from '../../components/user-menu'
 import { Accounts } from './components/accounts'
+import { DashboardProvider } from './components/dashboard-context'
 import { Transactions } from './components/transactions'
 
 export function Dashboard() {
   return (
-    <div className="w-full h-full p-4 md:p-8 md:pt-6 flex flex-col gap-4">
-      <header className="h-12 flex justify-between items-center">
-        <Logo className="h-6 text-teal-900" />
-        <UserMenu />
-      </header>
+    <DashboardProvider>
+      <div className="w-full h-full p-4 md:p-8 md:pt-6 flex flex-col gap-4">
+        <header className="h-12 flex justify-between items-center">
+          <Logo className="h-6 text-teal-900" />
+          <UserMenu />
+        </header>
 
-      <main className="flex-1 flex flex-col md:flex-row gap-4">
-        <section className="w-full md:w-1/2">
-          <Accounts />
-        </section>
-        <section className="w-full md:w-1/2">
-          <Transactions />
-        </section>
-      </main>
-    </div>
+        <main className="flex-1 flex flex-col md:flex-row gap-4 max-h-[calc(100%-65px)]">
+          <section className="w-full md:w-1/2">
+            <Accounts />
+          </section>
+          <section className="w-full md:w-1/2">
+            <Transactions />
+          </section>
+        </main>
+      </div>
+    </DashboardProvider>
   )
 }
