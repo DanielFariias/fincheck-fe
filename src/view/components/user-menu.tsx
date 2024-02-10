6ecf8fcf7ef546dd756c92bcf8c1c685a5f0f709
DanielFariias <danielfariias15@gmail.com>
2024-02-10 19:26:58 -0300

    refactor: user menu component to use dropdown menu

--- src/view/components/user-menu.tsx
+++ src/view/components/user-menu.tsx
@@ -1,9 +1,28 @@
+import { ExitIcon } from '@radix-ui/react-icons'
+import { DropdownMenu } from './dropdown-menu'
+import { useAuth } from '../../app/hooks/use-auth'
+
 export function UserMenu() {
+  const { signOut } = useAuth()
   return (
-    <div className="bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
-      <span className="text-sm tracking-[-0.5px] text-teal-900 uppercase ">
-        df
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
+          <span className="text-sm tracking-[-0.5px] text-teal-900 uppercase ">
+            df
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="w-32 mt-1">
+        <DropdownMenu.Item
+          className="flex items-center justify-between"
+          onSelect={signOut}
+        >
+          Sair
+          <ExitIcon className="w-4 h-4" />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   )
 }
