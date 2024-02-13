0de147ee361dfb0088144fe9eeaf20deb1bb134d
DanielFariias <danielfariias15@gmail.com>
2024-02-13 12:23:42 -0300

    feat: Add useDashboard hook to Fab component

--- src/view/pages/dashboard/components/fab.tsx
+++ src/view/pages/dashboard/components/fab.tsx
@@ -2,8 +2,11 @@ import { PlusIcon } from '@radix-ui/react-icons'
 import { DropdownMenu } from '../../../components/dropdown-menu'
 import { BankAccountIcon } from '../../../components/icons/bank-account-icon'
 import { CategoryIcon } from '../../../components/icons/categories/category-icon'
+import { useDashboard } from './dashboard-context/use-dashboard'
 
 export function Fab() {
+  const { openNewAccountModal, openNewTransactionModal } = useDashboard()
+
   return (
     <div className="fixed bottom-4 right-4">
       <DropdownMenu.Root>
@@ -14,15 +17,21 @@ export function Fab() {
         </DropdownMenu.Trigger>
 
         <DropdownMenu.Content className="w-56 mb-2">
-          <DropdownMenu.Item className="gap-2">
+          <DropdownMenu.Item
+            className="gap-2"
+            onSelect={() => openNewTransactionModal('EXPENSE')}
+          >
             <CategoryIcon type="expense" />
             Nova Despesa
           </DropdownMenu.Item>
-          <DropdownMenu.Item className="gap-2">
+          <DropdownMenu.Item
+            className="gap-2"
+            onSelect={() => openNewTransactionModal('INCOME')}
+          >
             <CategoryIcon type="income" />
             Nova Receita
           </DropdownMenu.Item>
-          <DropdownMenu.Item className="gap-2">
+          <DropdownMenu.Item className="gap-2" onSelect={openNewAccountModal}>
             <BankAccountIcon />
             Nova Conta
           </DropdownMenu.Item>
