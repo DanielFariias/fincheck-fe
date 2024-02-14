e2e80e48dfcf6f154935ca0945067267011732d7
DanielFariias <danielfariias15@gmail.com>
2024-02-14 15:04:00 -0300

    feat: Add variant prop to Button component

--- src/view/components/button.tsx
+++ src/view/components/button.tsx
@@ -4,6 +4,7 @@ import { Spinner } from './spinner'
 
 interface ButtonProps extends ComponentProps<'button'> {
   isLoading?: boolean
+  variant?: 'danger' | 'ghost'
 }
 
 export function Button({
@@ -11,6 +12,7 @@ export function Button({
   isLoading,
   disabled,
   children,
+  variant,
   ...props
 }: ButtonProps) {
   return (
@@ -18,12 +20,15 @@ export function Button({
       {...props}
       disabled={disabled || isLoading}
       className={cn(
-        'bg-teal-900 hover:bg-teal-800 w-full px-6 h-12 rounded-2xl text-white font-medium active:bg-teal-700 disabled:text-gray-500 disabled:bg-gray-200 disabled:cursor-not-allowed transition-all flex items-center justify-center',
+        'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 px-6 h-12 rounded-2xl font-medium text-white transition-all flex items-center justify-center',
+        variant === 'danger' && 'bg-red-900 hover:bg-red-800',
+        variant === 'ghost' &&
+          'bg-transparent border border-gray-800 text-gray-800 hover:bg-gray-800/5',
         className,
       )}
     >
       {!isLoading && children}
-      {isLoading && <Spinner className="w-6 h-6 fill-teal-700" />}
+      {isLoading && <Spinner className="w-6 h-6" />}
     </button>
   )
 }
