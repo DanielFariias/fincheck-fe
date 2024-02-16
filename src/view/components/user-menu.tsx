1dad6f6284acf01ff49e201fb52a861f61a32ab0
DanielFariias <danielfariias15@gmail.com>
2024-02-16 14:15:15 -0300

    Refactor: user menu component to include sign out and cache clearing functionality

--- src/view/components/user-menu.tsx
+++ src/view/components/user-menu.tsx
@@ -1,15 +1,23 @@
 import { ExitIcon } from '@radix-ui/react-icons'
 import { DropdownMenu } from './dropdown-menu'
 import { useAuth } from '../../app/hooks/use-auth'
+import { useQueryClient } from '@tanstack/react-query'
 
 export function UserMenu() {
-  const { signOut } = useAuth()
+  const { signOut, user } = useAuth()
+  const queryClient = useQueryClient()
+
+  function signOutAndClearCache() {
+    queryClient.clear()
+    signOut()
+  }
+
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
         <div className="bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
           <span className="text-sm tracking-[-0.5px] text-teal-900 uppercase ">
-            df
+            {user?.name.slice(0, 2).toUpperCase()}
           </span>
         </div>
       </DropdownMenu.Trigger>
@@ -17,7 +25,7 @@ export function UserMenu() {
       <DropdownMenu.Content className="w-32 mt-1">
         <DropdownMenu.Item
           className="flex items-center justify-between"
-          onSelect={signOut}
+          onSelect={signOutAndClearCache}
         >
           Sair
           <ExitIcon className="w-4 h-4" />
