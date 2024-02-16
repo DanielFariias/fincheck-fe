f234ec925c213a458b1f192b969946768ecd1e36
DanielFariias <danielfariias15@gmail.com>
2024-02-16 14:15:34 -0300

    Update favicon and add Google Fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+  },
+  resolve: {
+    alias: {
+      '@/app': path.resolve(__dirname, './src/app/'),
+      '@/assets': path.resolve(__dirname, './src/assets/'),
+      '@/router': path.resolve(__dirname, './src/router/'),
+      '@/view': path.resolve(__dirname, './src/view/'),
+    },
   },
 })
